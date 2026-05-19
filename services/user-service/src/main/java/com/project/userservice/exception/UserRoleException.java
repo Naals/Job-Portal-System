823ba83fd8de84eef6460bb8163f0e4d324c0186
Aslan <tynyshtykaslan06@gmail.com>
2026-05-19 22:16:55 +0500

    feat(user-service): exceptions

---
+++ services/user-service/src/main/java/com/project/userservice/exception/UserRoleException.java
@@ -0,0 +1,7 @@
+package com.project.userservice.exception;
+
+public class UserRoleException extends RuntimeException{
+    public UserRoleException(String message) {
+        super(message);
+    }
+}
