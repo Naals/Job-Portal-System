7e066f8731ec0d98635029f23d19b6b223af95bc
Aslan <tynyshtykaslan06@gmail.com>
2026-05-21 23:55:56 +0500

    feat(company-service): company service

---
+++ services/company-service/src/main/java/com/project/companyservice/service/CompanyService.java
@@ -0,0 +1,33 @@
+package com.project.companyservice.service;
+
+import com.project.common.domain.*;
+import com.project.common.dto.CompanyRequest;
+import com.project.common.dto.CompanyResponse;
+import com.project.companyservice.modal.Company;
+
+import java.util.List;
+
+public interface CompanyService {
+
+    CompanyResponse createCompany(Long ownerId, CompanyRequest req);
+
+    CompanyResponse getCompanyById(Long id);
+
+    CompanyResponse getMyCompany(Long ownerId);
+
+    List<CompanyResponse> getAllCompanies(
+            CompanyType companyType,
+            IndustryType industryType,
+            CompanyStatus companyStatus
+    );
+
+    CompanyResponse updateCompany(Long companyId, Long ownerId, CompanyRequest req);
+
+    CompanyResponse verifyCompany(Long companyId);
+
+    void deleteCompany(Long companyId);
+
+    CompanyResponse deactivateCompany(Long companyId);
+
+    Company getCompanyEntityById(Long id);
+}
