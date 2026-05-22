5e2588228a3aa58f5f87027f15f75dc45d1c992b
Aslan <tynyshtykaslan06@gmail.com>
2026-05-22 22:59:07 +0500

    feat(company-service): common dto

---
+++ common-lib/src/main/java/com/project/common/dto/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.project.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+
+    private String message;
+    private Boolean success;
+}
