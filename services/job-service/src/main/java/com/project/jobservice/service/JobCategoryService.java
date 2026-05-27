20c339eadd6adf0d59695064953f87ac51335e66
Aslan <tynyshtykaslan06@gmail.com>
2026-05-27 23:15:40 +0500

    feat(job-service): category service

---
+++ services/job-service/src/main/java/com/project/jobservice/service/JobCategoryService.java
@@ -0,0 +1,22 @@
+package com.project.jobservice.service;
+
+import com.project.common.dto.JobCategoryResponse;
+import com.project.jobservice.modal.JobCategory;
+import com.project.jobservice.payload.JobCategoryRequest;
+
+import java.util.List;
+
+public interface JobCategoryService {
+
+    JobCategoryResponse createCategory(JobCategoryRequest req);
+
+    List<JobCategoryResponse> getAllCategories();
+
+    JobCategoryResponse getCategoryById(Long id);
+
+    JobCategoryResponse updateCategory(Long id, JobCategoryRequest req);
+
+    void deleteCategory(Long id);
+
+    JobCategory getCategoryEntityById(Long id);
+}
