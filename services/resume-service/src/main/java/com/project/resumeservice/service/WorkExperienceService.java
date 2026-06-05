e2604f6a2a9edf14417a618fbd0263f9975b36bf
Aslan <tynyshtykaslan06@gmail.com>
2026-06-05 22:04:45 +0500

    feat(resume-service): work-exp service

---
+++ services/resume-service/src/main/java/com/project/resumeservice/service/WorkExperienceService.java
@@ -0,0 +1,17 @@
+package com.project.resumeservice.service;
+
+import com.project.common.dto.response.WorkExperienceResponse;
+import com.project.resumeservice.payload.AddWorkExperience;
+
+import java.util.List;
+
+public interface WorkExperienceService {
+
+    WorkExperienceResponse addWorkExperience(Long resumeId, Long candidateId, AddWorkExperience req);
+
+    List<WorkExperienceResponse> getWorkExperiences(Long resumeId);
+
+    WorkExperienceResponse updateWorkExperience(Long resumeId, Long workExperienceId, AddWorkExperience req);
+
+    void deleteWorkExperience(Long resumeId, Long workExperienceId, Long candidateId);
+}
