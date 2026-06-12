96c5ae9e6fdf49fae3f096ad45f3a1244bec08a5
Aslan <tynyshtykaslan06@gmail.com>
2026-06-11 23:40:12 -0400

    feat(resume-service): annotations

--- common-lib/src/main/java/com/project/common/dto/request/AddResumeSkillRequest.java
+++ common-lib/src/main/java/com/project/common/dto/request/AddResumeSkillRequest.java
@@ -5,7 +5,15 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AddResumeSkillRequest {
 
     @NotBlank(message = "Skill name is required")
